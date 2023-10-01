082f5fde36d68388bc184d8403a5b22f1db1b67a
Oleriiser <ole_riiser@hotmail.com>
2023-10-01 20:37:13 +0200

    added more gitignore

--- Lorafrom0.X/build/default/production/_ext/1824799377/system_assert.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/_ext/1824799377/system_assert.o: \
- ../src/config/default/Lora/sys/system_assert.c \
- ../src/config/default/Lora/sys/system_assert.h
-
-../src/config/default/Lora/sys/system_assert.h:
--- Lorafrom0.X/build/default/production/_ext/1824799377/system_init.o.d
+++
@@ -1,8 +0,0 @@
-build/default/production/_ext/1824799377/system_init.o: \
- ../src/config/default/Lora/sys/system_init.c \
- ../src/config/default/Lora/sys/system_init.h \
- ../src/config/default/Lora/sys/system_task_manager.h
-
-../src/config/default/Lora/sys/system_init.h:
-
-../src/config/default/Lora/sys/system_task_manager.h:
